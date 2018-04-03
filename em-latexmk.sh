4c9a514bc4ba5c2ed2a558499d2dde3d1abccc1f
yyh-gl <yhonda.95.gl@gmail.com>
2018-04-03 15:44:53 +0900

    texコンパイラを改善

--- em-latexmk.sh
+++ em-latexmk.sh
@@ -6,33 +6,34 @@ function tex(){
     ## a4以外で出力するときは外すらしい．
     VAR=`nkf -g ${FILENAME}.tex`
     if [ "${VAR}" = "Shift_JIS" ]; then
-		platex -kanji=sjis ${FILENAME}.tex
-		pbibtex -kanji=sjis ${FILENAME}
-		platex -kanji=sjis ${FILENAME}.tex
-		platex -kanji=sjis ${FILENAME}.tex
-		dvipdfmx -p a4 ${FILENAME}.dvi
-		open ${FILENAME}.pdf
+        platex -kanji=sjis ${FILENAME}.tex
+        pbibtex -kanji=sjis ${FILENAME}
+        platex -kanji=sjis ${FILENAME}.tex
+        platex -kanji=sjis ${FILENAME}.tex
+        dvipdfmx -p a4 ${FILENAME}.dvi
+        # open ${FILENAME}.pdf
     elif [ "${VAR}" = "EUC-JP" ]; then
-		platex -kanji=euc ${FILENAME}.tex
-		pbibtex -kanji=euc ${FILENAME}
-		platex -kanji=euc ${FILENAME}.tex
-		platex -kanji=euc ${FILENAME}.tex
-		dvipdfmx -p a4 ${FILENAME}.dvi
-		open ${FILENAME}.pdf
+        platex -kanji=euc ${FILENAME}.tex
+        pbibtex -kanji=euc ${FILENAME}
+        platex -kanji=euc ${FILENAME}.tex
+        platex -kanji=euc ${FILENAME}.tex
+        dvipdfmx -p a4 ${FILENAME}.dvi
+        # open ${FILENAME}.pdf
     elif [ "${VAR}" = "UTF-8" ]; then
-		platex -kanji=utf8 ${FILENAME}.tex
-		pbibtex -kanji=utf8 ${FILENAME}
-		platex -kanji=utf8 ${FILENAME}.tex
-		platex -kanji=utf8 ${FILENAME}.tex
-		dvipdfmx -p a4 ${FILENAME}.dvi
-		# open ${FILENAME}.pdf
+        platex -kanji=utf8 ${FILENAME}.tex
+        pbibtex -kanji=utf8 ${FILENAME}
+        platex -kanji=utf8 ${FILENAME}.tex
+        platex -kanji=utf8 ${FILENAME}.tex
+        dvipdfmx -p a4 ${FILENAME}.dvi
+        # open ${FILENAME}.pdf
     fi
     echo ${VAR}
-	rm -rf *.aux *.log *.dvi *.bbl *.blg
+    rm -rf *.aux *.log *.dvi *.bbl *.blg
 }
 
 ## YaTeX用 コンパイル & プレビュー プログラム
 if [ $# = 1 ]; then
-	tex $1 &&
-	echo $1 | sed -e "s/tex/pdf/" | xargs open -a preview
+    tex $1 &&
+    echo "${1%.*}.pdf" "pdf_${1%.*}.pdf" | xargs -n 2 mv &&
+    open -a preview "pdf_${1%.*}.pdf"
 fi
