b629680dc85cb2129bb3d6cdc7e314d9f328d767
yyh-gl <yhonda.95.gl@gmail.com>
2018-09-09 16:32:23 +0900

    makeSymbolicLink.sh：スペースを含むパスにも対応

--- makeSymbolicLink.sh
+++ makeSymbolicLink.sh
@@ -2,16 +2,16 @@
 
 # 現在ディレクトリの絶対パスを取得しDesktopにシンボリックリンクを作成
 if [ $# -eq 0 ]; then
-		ln -s $PWD/ ~/Desktop &&
+		ln -s "$PWD"/ ~/Desktop &&
 		echo "\nMaking Symbolic Link is Done"
 		exit 0
 elif [ $# -eq 1 ]; then
-		ln -s $PWD/ ~/Desktop/$1 &&
+		ln -s "$PWD"/ ~/Desktop/$1 &&
 		echo "\nMaking Symbolic Link is Done"
 		exit 0
 else
 		echo "[ERROR] Argument Error"
-		echo "mkln [ arg1 ]"
+		echo "mksl [ arg1 ]"
 		echo "arg1 : shortcut name"
 		exit 1
 fi
