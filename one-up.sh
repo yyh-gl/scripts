881c99aa5370c067c0721bdd8c502c69230280a2
yyh-gl <yhonda.95.gl@gmail.com>
2018-12-04 23:02:43 +0900

    修論バックアップ用スクリプトの表示文言を微修正

--- one-up.sh
+++ one-up.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-figlet -f slant "<<< Life Backup >>>" &&
+figlet -f slant "<<< One Up! >>>" &&
 rsync -ah --delete ~/Desktop/95_修士論文 ~/Desktop/01_ISDL/03_Conference/ &
 rsync -ah ~/Desktop/95_修士論文 ~/OneDrive\ -\ 同志社大学/同期フォルダ/ &
 progress -m &&
-figlet -f slant "  <<< All Done >>>"
+figlet -f slant "<<< All Done >>>"
