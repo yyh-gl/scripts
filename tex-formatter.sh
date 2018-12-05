fcb57314291faa36643e9a5f2cbb62bad1d8710b
yyh-gl <yhonda.95.gl@gmail.com>
2018-12-05 16:05:06 +0900

    TeX用フォーマット整形スクリプトを追加

---
+++ tex-formatter.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+## 「、。」を「，．」に変換するスクリプト
+
+# paper.texを変換
+echo "Translate -> ./paper.tex" &&
+sed -i".backup" -e "s/、/，/g" -e "s/。/．/g" ./paper.tex
+
+# chapter内のtexファイルを変換
+for file in `\find ./chapter -name '*.tex'`; do
+    echo "Translate -> $file" &&
+    sed -i".backup" -e "s/、/，/g" -e "s/。/．/g" $file
+done
+
+# バックアップ用ファイルをバックアップディレクトリに移動
+mv -f ./paper.tex.backup ./SedBackup/ &&
+mv -f ./chapter/*.backup ./SedBackup/
