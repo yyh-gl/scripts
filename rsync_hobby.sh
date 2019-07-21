4dd7398d697900af6b435b7f6d2e650926d9bf7c
yyh-gl <yhonda.95.gl@gmail.com>
2019-07-21 19:44:01 +0900

    バックアップスクリプトをバックアップ用HDDにも対応

--- rsync_hobby.sh
+++ rsync_hobby.sh
@@ -1,20 +1,40 @@
 echo "===================================================" &&
     echo "[RSYNC] START" &&
     echo "===================================================" &&
+    # /hobbyの内容をトランセンドにバックアップ（シンボリックリンクを除く）
     echo "\n===================================================" &&
     echo "[RSYNC] ~/Desktop/hobby/ -> /Volumes/TRANSCEND" &&
     echo "===================================================" &&
-    sudo rsync -auv --delete --exclude '02_ISDL' --exclude '.DS_Store' \
+    sudo rsync -auv --delete \
+         --exclude '01_CasualLife/01_書類' \
+         --exclude '02_ISDL'\
+         --exclude '.DS_Store' \
          ~/Desktop/hobby/ \
-         /Volumes/TRANSCEND
-    echo "\n===================================================" &&
+         /Volumes/TRANSCEND &&
+    # /Documents（書類）の内容をトランセンドにバックアップ
+    echo "\n===============================================================" &&
+    echo "[RSYNC] ~/Documents/ -> /Volumes/TRANSCEND/01_CasualLife/01_書類" &&
+    echo "=================================================================" &&
+    sleep 3 &&
+    sudo rsync -auv --exclude '.DS_Store' \
+         ~/Documents/ \
+         /Volumes/TRANSCEND/01_CasualLife/01_書類 &&
+    # トランセンドのバックアップ内容をNasneにバックアップ
+    echo "\n==============================================================" &&
     echo "[RSYNC] /Volumes/TRANSCEND/ -> /Volumes/share1/Backup/Transcend" &&
-    echo "===================================================" &&
+    echo "==============================================================" &&
     sleep 3 &&
     sudo rsync -auv --exclude '.DS_Store' \
          /Volumes/TRANSCEND/ \
-         /Volumes/share1/Backup/Transcend
+         /Volumes/share1/Backup/Transcend &&
+    # トランセンドのバックアップ内容をHDDにバックアップ
+    echo "\n==============================================================" &&
+    echo "[RSYNC] /Volumes/TRANSCEND/ -> /Volumes/HDD_U_01_Main/99_Backup/01_Transcend" &&
+    echo "==============================================================" &&
+    sleep 3 &&
+    sudo rsync -auv --exclude '.DS_Store' \
+         /Volumes/TRANSCEND/ \
+         /Volumes/HDD_U_01_Main/99_Backup/01_Transcend &&
     echo "\n===================================================" &&
     echo "[RSYNC] DONE" &&
     echo "==================================================="
-
